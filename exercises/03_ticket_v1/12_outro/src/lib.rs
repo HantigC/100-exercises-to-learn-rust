2b37b7d6691fb176527d84e1ba18774e2107416e
HantigC <cristianhantig96@gmail.com>
2026-08-12 13:44:20 +0300

    solve 03/12

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,61 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+pub struct Order {
+    product_name: String,
+    quantity: i32,
+    unit_price: i32,
+}
+
+impl Order {
+    fn check_product_name(product_name: &str) {
+        if product_name.is_empty() {
+            panic!("")
+        }
+        if product_name.len() > 300 {
+            panic!("")
+        }
+    }
+    fn check_quantity(quantity: i32) {
+        if quantity <= 0 {
+            panic!("")
+        }
+    }
+    fn check_unit_price(unit_price: i32) {
+        if unit_price <= 0 {
+            panic!("")
+        }
+    }
+
+    pub fn product_name(&self) -> &str {
+        &self.product_name
+    }
+    pub fn quantity(&self) -> &i32 {
+        &self.quantity
+    }
+    pub fn unit_price(&self) -> &i32 {
+        &self.unit_price
+    }
+    pub fn set_product_name(&mut self, new_product_name: String) {
+        self.product_name = new_product_name;
+    }
+    pub fn set_quantity(&mut self, new_quantity: i32) {
+        self.quantity = new_quantity
+    }
+    pub fn set_unit_price(&mut self, new_unit_price: i32) {
+        self.unit_price = new_unit_price
+    }
+    pub fn total(&self) -> i32 {
+        self.unit_price * self.quantity
+    }
+    pub fn new(product_name: String, quantity: i32, unit_price: i32) -> Order {
+        Order::check_product_name(&product_name);
+        Order::check_quantity(quantity);
+        Order::check_unit_price(unit_price);
+        Order {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+}
