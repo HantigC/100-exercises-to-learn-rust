4cead343f461bb9d1dba30e1b96ceed980ffc900
HantigC <cristianhantig96@gmail.com>
2026-08-27 17:36:38 +0300

    solve 05/05

--- exercises/05_ticket_v2/05_nullability/src/lib.rs
+++ exercises/05_ticket_v2/05_nullability/src/lib.rs
@@ -1,5 +1,7 @@
 // TODO: Implement `Ticket::assigned_to` using `Option` as the return type.
 
+use crate::Status::InProgress;
+
 #[derive(Debug, PartialEq)]
 struct Ticket {
     title: String,
@@ -36,7 +38,12 @@ impl Ticket {
         }
     }
     pub fn assigned_to(&self) -> Option<&String> {
-        todo!()
+        let maybe_assigned_to = if let Status::InProgress { assigned_to } = &self.status {
+            Some(assigned_to)
+        } else {
+            None
+        };
+        maybe_assigned_to
     }
 }
 
