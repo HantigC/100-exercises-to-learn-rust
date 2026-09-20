8dccace51b8dd96ea69bd45a98beb23e8c829109
HantigC <cristianhantig96@gmail.com>
2026-09-20 13:30:34 +0300

    solve 06/02

--- exercises/06_ticket_management/02_vec/src/lib.rs
+++ exercises/06_ticket_management/02_vec/src/lib.rs
@@ -11,11 +11,14 @@
 // We expect `fibonacci(0)` to return `0`, `fibonacci(1)` to return `1`,
 // `fibonacci(2)` to return `1`, and so on.
 pub fn fibonacci(n: u32) -> u32 {
-    // TODO: implement the `fibonacci` function
-    //
-    // Hint: use a `Vec` to memoize the results you have already calculated
-    // so that you don't have to recalculate them several times.
-    todo!()
+    let mut fib_sequence: Vec<u32> = Vec::new();
+    fib_sequence.push(0);
+    fib_sequence.push(1);
+    let n_usize = n as usize;
+    for index in 2..n_usize +1 {
+        fib_sequence.push(fib_sequence[index-1] + fib_sequence[index-2]);
+    }
+    fib_sequence[n_usize]
 }
 
 #[cfg(test)]
